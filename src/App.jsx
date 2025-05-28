9c160528a9d3056742e6a40eae7df6904635e181
AzimAminz <anakuazim10@gmail.com>
2025-05-28 13:57:06 +0800

    test about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home.jsx'
 import NotFound from './pages/NotFound.jsx';
+import About from './pages/About.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path='/about'  element={<About/>}/>
           <Route path="*" element={<NotFound />} />
         </Routes>
     </BrowserRouter>
